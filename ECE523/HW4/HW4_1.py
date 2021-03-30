de15d04c2abdf7cfd42b04fefee0e94093b63ed9
Sam Freitas <60946332+SamFreitas1996@users.noreply.github.com>
2021-03-30 14:47:51 -0700

    Add files via upload

--- ECE523/HW4/HW4_1.py
+++ ECE523/HW4/HW4_1.py
@@ -1,4 +1,8 @@
-#HW 4 problem 1
+# HW 4 problem 1
+# Samuel Freitas
+# ECE 523 
+
+# reference: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
 import numpy as np 
 import os
@@ -10,10 +14,14 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as nn_func
 import torch.optim as optim
-
+from tabulate import tabulate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
+if torch.cuda.is_available() :
+    print('Using GPU:', torch.cuda.get_device_name(torch.cuda.current_device()))
+else:
+    print('Using the cpu')
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -37,9 +45,9 @@ classes = ('plane', 'car', 'bird', 'cat',
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 12, 5)
+        self.conv1 = nn.Conv2d(3, 32, 5,stride = 1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(12, 16, 5)
+        self.conv2 = nn.Conv2d(32, 16, 5, stride = 1)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
@@ -47,121 +55,97 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(nn_func.relu(self.conv1(x)))
         x = self.pool(nn_func.relu(self.conv2(x)))
+        # x = self.pool(nn_func.relu(self.conv3(x)))
         x = x.view(-1, 16 * 5 * 5)
         x = nn_func.relu(self.fc1(x))
         x = nn_func.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-
-
-def imshow_custom(img,fig_number,fig_title):
-    plt.ion()
-    img = (img / 2 + 0.5).cpu()     # unnormalize
-    npimg = img.numpy()
-    fig_n = plt.figure(fig_number)
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.title(fig_title)
-    fig_n.show()
-
-
+# set the network and point it to the GPU
 net = Net()
 net.to(device)
+
+# save a blank network
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
 
 # get some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
-# print labels
-this_title = (' '.join('%5s' % classes[labels[j]] for j in range(4)))
-# show images
-# imshow_custom(torchvision.utils.make_grid(images),1,this_title)
-
+# loss funtion selection
 criterion = nn.CrossEntropyLoss()
-# weight_decay is L2 nrom
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9,weight_decay=1e-2)
 
+# create some variables for the loop
+# weight_decay is L2 norm
+L2_weight_penalty_list = [0,1e-5,0,1e-5]
+number_epochs_list = [2,2,10,10]
+results = []
 
-for epoch in range(2):  # loop over the dataset multiple times
+for i in range(4):
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        # inputs, labels = data
-        inputs, labels = data[0].to(device), data[1].to(device)
+    L2_weight_penalty = L2_weight_penalty_list[i]
+    learning_rate = 0.001
+    momentum_rate = 0.9
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum_rate,weight_decay=L2_weight_penalty)
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+    net.load_state_dict(torch.load(PATH))
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    number_epochs = number_epochs_list[i]
+    for epoch in range(number_epochs):  # loop over the dataset multiple times
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            # inputs, labels = data
+            inputs, labels = data[0].to(device), data[1].to(device)
 
-print('Finished Training')
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
+            # print statistics
+            running_loss += loss.item()
+            if i % 12000 == 11999:    # print every 12000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                    
+                end_running_loss = running_loss
+                running_loss = 0.0
 
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-images, labels = images.cuda(), labels.cuda()
+    print('Finished Training')
 
-net = Net()
-net.to(device)
-PATH = './cifar_net.pth'
-net.load_state_dict(torch.load(PATH))
-outputs = net(images).to(device)
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+    images, labels = images.cuda(), labels.cuda()
 
+    # net = Net()
+    # net.to(device)
+    # PATH = './cifar_net.pth'
+    # net.load_state_dict(torch.load(PATH))
+    outputs = net(images).to(device)
 
-_, predicted = torch.max(outputs, 1)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = net(images).to(device)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-this_title = ('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
+    acc_correct = 100 * correct / total
 
-# imshow_custom(torchvision.utils.make_grid(images),2,this_title)
+    print('Accuracy:', acc_correct, '% - L2 weight penalty:',L2_weight_penalty, ' - Epochs:', number_epochs)
 
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(device), data[1].to(device)
-        outputs = net(images).to(device)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    this_result = [number_epochs,L2_weight_penalty,acc_correct,end_running_loss]
+    results.append(this_result)
 
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(device), data[1].to(device)
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-
-plt.ioff()
-plt.show()
+print(tabulate(results, headers=['Num epochs','L2 penalty','Accuracy','loss']))
