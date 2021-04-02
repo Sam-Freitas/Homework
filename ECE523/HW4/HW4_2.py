f88ffe07722825f25579403924bc1be2cc91a9b1
sam freitas <samfreitas@dhcp-10-132-234-193.uawifi.arizona.edu>
2021-04-02 11:40:38 -0700

    part 2

--- ECE523/HW4/HW4_2.py
+++ ECE523/HW4/HW4_2.py
@@ -16,10 +16,12 @@ implementation on datasets from the course Github page.
 import numpy as np
 
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.datasets import make_classification
-
+from sklearn.datasets import make_classification,make_multilabel_classification,make_sparse_spd_matrix
+import matplotlib.pyplot as plt
 
 import numpy as np
+from numpy.random import multivariate_normal as N
+
 
 # Decision stump used as weak classifier
 class DecisionStump():
@@ -30,13 +32,22 @@ class DecisionStump():
         self.alpha = None
 
     def predict(self, X):
+
+        M = X.shape[0]
+        N_ints = np.random.randint(1,M+1, M)
+        p_uniform = np.array(np.ones((np.max(N_ints),))/np.max(N_ints))
+
+        rand_choice = np.random.choice(np.arange(1, len(p_uniform)+1), size=1,p=p_uniform) 
+
+        this_threshold = X[rand_choice][0][0]
+
         n_samples = X.shape[0]
         X_column = X[:, self.feature_idx]
         predictions = np.ones(n_samples)
         if self.polarity == 1:
-            predictions[X_column < self.threshold] = -1
+            predictions[X_column < this_threshold] = -1
         else:
-            predictions[X_column > self.threshold] = -1
+            predictions[X_column > this_threshold] = -1
 
         return predictions
 
@@ -65,7 +76,7 @@ class Adaboost():
 
                 for threshold in thresholds:
                     # predict with polarity 1
-                    p = 1
+                    p = .5
                     predictions = np.ones(n_samples)
                     predictions[X_column < threshold] = -1
 
@@ -104,3 +115,47 @@ class Adaboost():
         y_pred = np.sign(y_pred)
 
         return y_pred
+
+
+def plot_2D_labeled_data(X,y,fig_number,fig_title):
+    # put plt.ioff() and plt.show() at end 
+    plt.ion()
+    f = plt.figure(fig_number)
+    plt.scatter(X[:,0],X[:,1],c=y)
+    plt.axis('equal')
+    plt.title(fig_title)
+    f.show()
+
+def gen_multilabel_data(num_labels,num_samples_per,size_constraint):
+    for i in range(num_labels):
+        this_center = np.random.randint(size_constraint,size=2)
+        # this_cov_array = make_sparse_spd_matrix(2)
+        this_cov_array = np.array([[1,0],[0,1]])
+
+        dX = N(this_center,this_cov_array,size = num_samples_per)
+        dy = np.zeros((1,num_samples_per))+i
+        
+        if i == 0:
+            X = dX
+            y = dy
+        else:
+            X = np.append(X,dX,0)
+            y = np.append(y,dy)
+
+    return(X,y)
+
+X,y = gen_multilabel_data(num_labels = 2,num_samples_per = 100,size_constraint = 4)
+
+plot_2D_labeled_data(X,y,1,"Given data")
+
+clf = Adaboost()
+
+clf.fit(X,y)
+
+y2 = clf.predict(X)
+
+plot_2D_labeled_data(X,y2,2,"Adaboost data")
+
+print("end")
+plt.ioff()
+plt.show()
