44abda8070264d46c49b225d4308047293d21de3
Sam Freitas <60946332+SamFreitas1996@users.noreply.github.com>
2021-04-26 13:55:13 -0700

    Add files via upload

--- ECE523/HW5/HW5_1.py
+++ ECE523/HW5/HW5_1.py
@@ -1,42 +1,51 @@
-# HW5 problem 1
-# Samuel Freitas
-# ECE 523
-
-"""
-Generate 2D Gaussian data that is similar to the data set shown in Figure 1. 
-
-Using data sets that have 1000 samples (500 from each class) implement the self-training algorithm, 
-and test on a dataset of 1000 samples. The requirements for this problem are as follows:
-
-•Your implementation of self-training must use a classifier (e.g., neural network) that can give probabilities 
-    to select the data points that will have pseudo labels assigned to them. 
-    I will not make a restriction on the classifier other than the probabilities requirement. 
-    You will need to choose a suitable threshold to determine the data samples 
-    that will be labeled for the nextround of self-training.
-•Report the error of the self-training algorithm on the testing data at: 
-    (1) the first time a classifier is trained using only the labeled; 
-    (2) at least one timepoint during the self training process (i.e., when pseudo labels are used); and 
-    (3) after self-training is completed. Comment on the results.
-•Perform an experiment reporting the above requirements with 10% and when 25% of the training data are labeled.
-"""
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.layers as layers
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import confusion_matrix
 import numpy as np 
 import matplotlib.pyplot as plt
-
-import torch
-from torch import optim
-import torch.nn as nn
-import torch.nn.functional as F
-from fit_module import FitModule
+import matplotlib.cm as cm
+import pandas as pd
+import random
 
 def plot_2D_labeled_data(X,y,fig_number,fig_title):
     # put plt.ioff() and plt.show() at end 
+
+    colors = cm.cool(y/max(y))
+
     plt.ion()
     f = plt.figure(fig_number)
-    plt.scatter(X[:,0],X[:,1],c=y)
-    plt.axis('equal')
+    plt.scatter(X[:,0],X[:,1],c=colors,alpha=0.7)
+    # plt.axis('tight')
     plt.title(fig_title)
     f.show()
+    return fig_number+1
+
+def anomalyScores(originalDF, reducedDF):
+    loss = np.sum((np.array(originalDF) - \
+                   np.array(reducedDF))**2, axis=1)
+    
+    print('Mean for anomaly scores: ', np.mean(loss))
+    
+    return loss 
+
+def get_acc(predictions,labels):
+
+    conf_mat = confusion_matrix(predictions, labels)
+    acc = np.sum(conf_mat.diagonal()) / np.sum(conf_mat)
+
+    return acc
+
+def covert_to_lables(predictions):
+
+    labels = np.zeros(shape=(len(predictions)))
+
+    for count, this_prediction in enumerate(predictions):
+        labels[count] = np.argmax(this_prediction)+1
+    return labels
 
 # generate data constraints
 mean1 = [-1.1,-1.1]
@@ -53,45 +62,80 @@ d2 = np.random.multivariate_normal(mean2, cov, num_points_per)
 l1 = np.zeros((1,num_points_per))+1
 l2 = np.zeros((1,num_points_per))+2
 
-# plot the training data
-plot_2D_labeled_data(np.append(d1,d2,axis = 0),np.append(l1,l2),1,"Training data")
+unlabeled_data = np.append(d1,d2,axis=0)
+labels = np.append(l1,l2)
 
-# create the unlabeled dataset
-unlabeled_data = np.append(d1,d1,axis=0)
+# plot the training data
+fig_num = plot_2D_labeled_data(unlabeled_data,labels,1,"Training data")
+
+# create labels
+unique_labels = np.unique(labels)
+unique_labels2 = np.append(unique_labels,np.max(unique_labels)+1)
+
+binned_labels = label_binarize(labels,classes= unique_labels2)
+binned_labels = binned_labels[:,0:int(np.max(unique_labels))]
 
 input_size = unlabeled_data.shape
+label_size = binned_labels.shape
+print("input shape",input_size)
 
+# Call neural network API
+model = tf.keras.Sequential()
+# Apply linear activation function to input layer
+# Generate hidden layer with 14 nodes, the same as the input layer
+model.add(layers.Dense(units=128, activation='linear',input_dim=input_size[1]))
+model.add(layers.Dense(units=15, activation='relu'))
+model.add(layers.Dropout(0.2))
+# Apply linear activation function to hidden layer
+# Generate output layer with 14 nodes
+model.add(layers.Dense(units=label_size[1], activation='sigmoid'))
+# Compile the model
+model.compile(optimizer='SGD',
+              loss='CategoricalCrossentropy',
+              metrics=['accuracy'])
 
-        
-model = Net()
-            
-# criterion1 = nn.MSELoss()
-# criterion2 = nn.BCELoss()
-            
-# optimizer = optim.Adam(model.parameters(), lr=0.01)
+model.save('my_model.h5')
 
-# epochs = 150
-   
-# for epoch in range(epochs): 
-#     for inputs, labels in train_loader:
-#         inputs = Variable(inputs)
-#         labels = Variable(labels)       
-#         optimizer.zero_grad()
-#         decoded, out = model(inputs)
-       
-#         loss1 = criterion1(decoded, inputs) 
-#         loss2 = criterion2(out, labels)
-#         loss = loss1 + loss2
-#         loss.backward()
-#         optimizer.step()
+callback = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=10,restore_best_weights=True)
 
+# Train the model
+num_epochs = 50
+batch_size = 256
 
+percent_given_labels = [.1,.25]
 
+for count, val in enumerate(percent_given_labels):
 
+    num_to_give_labels = round(len(unlabeled_data)*val)
+    idx_list = list(range(len(unlabeled_data)))
 
+    idx = random.choices(idx_list,k=num_to_give_labels)
+
+    this_X = unlabeled_data[idx,:]
+    this_y = binned_labels[idx]
+
+    model = tf.keras.models.load_model('my_model.h5')
+
+    history = model.fit(x=this_X, y=this_y,
+                        epochs=num_epochs,
+                        batch_size=batch_size,
+                        shuffle=True,
+                        validation_data=(this_X, this_y),
+                        verbose=0,
+                        callbacks=[callback])
+    
+    predictions = model.predict(unlabeled_data)
+
+    prediction_labels = covert_to_lables(predictions)
+
+    this_accuracy = get_acc(prediction_labels,labels)
+
+    print('Using',val,'of the data the model is',this_accuracy)
+
+    fig_title = "Predictions using" + str(val) + " of data with acc:" + str(this_accuracy) + "accurate"
+
+    fig_num = plot_2D_labeled_data(unlabeled_data,prediction_labels,fig_num,fig_title)
 
 
 plt.ioff()
 plt.show()
-
-print("end")
